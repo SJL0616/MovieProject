d7d1ec7b82187a764581b6a1bbc8f2e5c3e7405b
wonusuh <wonwooseo23@gmail.com>
2024-02-26 15:54:19 +0900

    updated : 회원가입 기능 추가 (css 보류)

--- src/movie/controller/user/UserIdValidAjax.java
+++ src/movie/controller/user/UserIdValidAjax.java
@@ -1,27 +1,24 @@
 package movie.controller.user;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import movie.controller.Controller;
 import movie.dao.UserDAO;
+import movie.vo.UserVO;
 
-public class UserJoinController implements Controller {
+public class UserIdValidAjax implements Controller {
 	@Override
 	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String inputId = request.getParameter("id");
-		if (inputId == null || inputId.equals(""))
-			return "userJoin";
-		String inputPw = request.getParameter("pw");
-		String inputEmail = request.getParameter("email");
-		String inputPhone = request.getParameter("phone");
-		UserDAO.getInstance().addUserIntoDB(inputId, inputPw, inputEmail, inputPhone);
-		String ctx = request.getContextPath();
-		return "redirect:" + ctx + "/index.jsp";
+		UserVO user = UserDAO.getInstance().getTheUserById(inputId);
+		String passData = "notValid";
+		if (user == null)
+			passData = "valid";
+		response.getWriter().print(passData);
+		return null;
 	}
 }
--- src/movie/dao/UserDAO.java
+++ src/movie/dao/UserDAO.java
@@ -1,15 +1,17 @@
 // 작성자 : 서원우
 // 내용 : 유저 dao 입니다.
 // 최초 작성일 : 24-02-22
-// 최종 수정일 : 24-02-22 (서원우)
+// 최종 수정일 : 24-02-26 (서원우)
 package movie.dao;
 
 import java.io.IOException;
 import java.io.InputStream;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import movie.vo.UserVO;
 
 public class UserDAO {
@@ -36,18 +38,34 @@ public class UserDAO {
 	public UserVO getTheUserById(String id) {
 		SqlSession session = sqlSessionFactory.openSession();
 		UserVO user = session.selectOne("getTheUserById", id);
+		session.close();
 		return user;
 	}
 
 	public UserVO getTheUserByKakaoId(String kakaoId) {
 		SqlSession session = sqlSessionFactory.openSession();
 		UserVO user = session.selectOne("getTheUserByKakaoId", kakaoId);
+		session.close();
 		return user;
 	}
 
 	public UserVO getTheUserByNaverId(String naverId) {
 		SqlSession session = sqlSessionFactory.openSession();
 		UserVO user = session.selectOne("getTheUserByNaverId", naverId);
+		session.close();
 		return user;
+	}
+
+	public int addUserIntoDB(String inputId, String inputPw, String inputEmail, String inputPhone) {
+		UserVO user = new UserVO();
+		user.setId(inputId);
+		user.setPw(inputPw);
+		user.setEmail(inputEmail);
+		user.setPhone(inputPhone);
+		SqlSession session = sqlSessionFactory.openSession();
+		int cnt = session.insert("addUserIntoDB", user);
+		session.commit();
+		session.close();
+		return cnt;
 	}
 }
