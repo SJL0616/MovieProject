4ef26737d02393366ef2bda296323479f37cf86b
SJL0616 <81796008+SJL0616@users.noreply.github.com>
2024-03-06 15:00:22 +0900

    세션값 확인 로직 추가_0306_이상준

--- WebContent/js/header.js
+++ WebContent/js/header.js
@@ -1,14 +1,22 @@
+let sessionLog = null;
+$(document).ready(function() {
+	//데이트피커 초기화let
+	sessionLog = document.getElementById("session").value;
+}); // document ready function
+
 function toMyMega() {
-	fetch("checkLog.do", {
-		method: "POST",
-		headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", },
-	})
-		.then(response => response.text())
-		.then((data) => {
-			if (data === `fail`) {
-			alert(`로그인이 필요한 메뉴입니다.`);
-			} else {
-				location.href = "http://localhost:8085/MovieProject/userMyMega.do";
-			}
-		})
+	if(isLoggedIn()){
+	    location.href = "http://localhost:8085/MovieProject/userMyMega.do";
+	}else{
+		alert(`로그인이 필요한 메뉴입니다.`);
+	}
+}
+
+function isLoggedIn(){
+	console.log("현재 세션값 : "+sessionLog);
+	if(sessionLog != '' && sessionLog != undefined && sessionLog != null){
+	    return true;
+	}else{
+		return false;
+	}
 }
